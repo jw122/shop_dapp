e7ad51d98fed1c00f47fd1928f04dc17d516c74b
jw122 <jwu2802@gmail.com>
2017-07-23 13:35:34 -0300

    front end: adding to cart, updating cart info

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,10 +6,12 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import shop_artifacts from '../../build/contracts/Shop.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+var Shop = contract(shop_artifacts);
+
+// A list to represent the cart array from the contract
+let cartItems = {}
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
@@ -21,8 +23,7 @@ window.App = {
   start: function() {
     var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+    Shop.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
@@ -39,52 +40,119 @@ window.App = {
       accounts = accs;
       account = accounts[0];
 
+      // Upon launching the window, we want to refresh the buyer's balance and update the cart.
       self.refreshBalance();
+      self.populateCart();
+      self.updateCartTotals();
     });
   },
 
+  /* This function will call web3.eth.getBalance to retrive the current balance, in Wei, 
+  of the selected account. */
   refreshBalance: function() {
     var self = this;
+    var deployedContract;
+    var balance;
 
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
+    Shop.deployed().then(function(instance){
+      deployedContract = instance;
+      return web3.eth.getBalance(account, function(err, bal) {
+
+        if (err!= null) {
+          alert("There was an error getting your balance.");
+          return;
+        }
+
+        balance = bal;
+        var balance_element = document.getElementById("balance");
+        balance_element.innerHTML = balance.toNumber();
+
+      })
+
+      }).catch(function(e){
+        console.log(e);
+      });
+
+  },
+
+  /* This function will trigger the getCartItems() function in the contract to retrive the cart array,
+  and then loop through the array to populate the cartItems array in this .js environment. */
+  populateCart: function() {
+    var self = this;
+    var deployedContract;
+
+    Shop.deployed().then(function(instance) {
+      deployedContract = instance;
+      deployedContract.getCartItems().then(function(cartArray){
+        console.log(cartArray)
+        for (let i=0; i < cartArray.length; i++) {
+          /*We had stored item names as byes32 on the blockchain,
+          so we'll use 'toUtf8' to convert bytes32 back to strings*/
+          cartItems[web3.toUtf8(cartArray[i])] = "cartItem-" + i;
+        }
+        // Once we're done retrieving cart items, we use the array to fill out the rows in the front-end
+        // table.
+        self.setupCartRows();
+      });
     });
   },
 
-  sendCoin: function() {
-    var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
+  /* This function goes through each item in the cartItems array and places it in the HTML table. */
+  setupCartRows: function() {
+    Object.keys(cartItems).forEach(function (item) { 
+      $("#cartItem-rows").append("<tr><td>" + item + "</td><td id='" + cartItems[item] + "'></td></tr>");
     });
-  }
+  },
+
+  /* This function calls on the contracts getCartItems() and getTotalPrice() functions to retrieve
+  the cart's totals. Then it udpates the front-end elements accordingly. */
+  updateCartTotals: function() {
+    var self = this;
+    var deployedContract;
+
+    Shop.deployed().then(function(instance){
+      deployedContract=instance;
+
+      deployedContract.getCartItems().then(function(cartArray){
+        console.log("cart length: ", cartArray.length);
+        var total_items_element = document.getElementById("totalItems");
+        total_items_element.innerHTML = cartArray.length;
+      })
+
+      deployedContract.getTotalPrice().then(function(totalPrice){
+        console.log("total price: ", totalPrice.toNumber());
+        var total_price_element = document.getElementById("totalPrice");
+        total_price_element.innerHTML = totalPrice.toNumber();
+      })
+    })
+  },
+
+  /* Add the given item into the cart from the contract, then update the cart information in front-end*/
+  addToCart: function(item) {
+    var self = this;
+    var deployedContract;
+
+    Shop.deployed().then(function(instance){
+      deployedContract = instance;
+
+      return deployedContract.addItem(item, {from: account});
+    }).then(function(value){
+      console.log("item added");
+      populateCart();
+      updateCartTotals();
+
+    }).catch(function(e){
+      console.log(e);
+    });
+
+  },
+
 };
 
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 Wei, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -7,11 +7,14 @@ contract Shop {
     address public buyer; // Buyer's address, in this case also msg.sender
     address public seller; // Seller's address, aka store owner
     uint public currentTotal; // We'll use a uint to keep track of the cart's total price
-    bytes32[] public cart; // Buyer's list of items, aks cart
+    bytes32[] public cart; // Buyer's list of items, aka cart: an array of bytes32
     mapping (bytes32 => uint) public priceTable; // We use a map to keep track of each item's price
 
+  /* The constructor will take in the seller's address as a parameter. It will also initialize the buyer, seller, 
+  currentTotal and priceTable variables. The constructor will be called once when you deploy the contract
+  to the blockchain. */
   function Shop(address _seller) {
-    // constructor
+    
     buyer = msg.sender;
     seller = _seller;
     currentTotal = 0;
@@ -26,15 +29,19 @@ contract Shop {
     
   }
   
+  /* This function will add the item passed in as an argument to the cart, by pushing it into the
+  car array. Then, it will update the total price of the cart based on the current item's price. */
   function addItem(bytes32 item) {
       cart.push(item);
       currentTotal = currentTotal + priceTable[item];
   }
   
+  /* This function simply returns the cart array itself */
   function getCartItems() constant returns (bytes32[]){
       return cart;
   }
   
+  /* This function returns the current total price of the cart */
   function getTotalPrice() constant returns (uint) {
       return currentTotal;
   }
